7b20a1f4c663077c4eb327565f896fed5e448d72
David Yang <dayang@google.com>
2016-09-15 09:49:53 -0700

    Feat(UI): Single Thread View
    Basic UI widgets for showing a single thread.
    
    Change-Id: I400098708a5a610387cb265737a06a94bfdb8e56

---
+++ packages/widgets/lib/email/message_action_bar_footer.dart
@@ -0,0 +1,114 @@
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+import 'package:models/email/message.dart';
+
+import 'type_defs.dart';
+
+/// Footer for a [Message] that contains quick actions
+/// Actions are Reply, ReplyAll, and Foward
+/// This footer will be typically found at the end of a individual ThreadView
+class MessageActionBarFooter extends StatelessWidget {
+  /// Given thread associated with this action bar
+  Message message;
+
+  /// Callback for selecting forward
+  MessageActionCallback onForwardMessage;
+
+  /// Callback for selecting reply all
+  MessageActionCallback onReplyAllMessage;
+
+  /// Callback for selecting reply
+  MessageActionCallback onReplyMessage;
+
+  /// Constructor to create a [MessageActionBarFooter]
+  MessageActionBarFooter(
+      {Key key,
+      @required this.message,
+      @required this.onForwardMessage,
+      @required this.onReplyAllMessage,
+      @required this.onReplyMessage})
+      : super(key: key) {
+    assert(message != null);
+    assert(onForwardMessage != null);
+    assert(onReplyAllMessage != null);
+    assert(onReplyMessage != null);
+  }
+
+  void _onForwardMessage() {
+    onForwardMessage(message);
+  }
+
+  void _onReplyAllMessage() {
+    onReplyAllMessage(message);
+  }
+
+  void _onReplyMessage() {
+    onReplyMessage(message);
+  }
+
+  Widget _buildIconWithText({String text, IconData icon}) {
+    return new Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        new Icon(icon),
+        new Container(
+          margin: const EdgeInsets.only(top: 8.0),
+          child: new Text(
+            text,
+            style: new TextStyle(
+              fontSize: 12.0,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Material(
+      color: Colors.white,
+      child: new ButtonBar(
+        alignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          new MaterialButton(
+            child: _buildIconWithText(
+              text: 'REPLY',
+              icon: Icons.reply,
+            ),
+            color: Colors.white,
+            textColor: Colors.grey[400],
+            onPressed: _onReplyMessage,
+            height: 50.0,
+          ),
+          new MaterialButton(
+            child: _buildIconWithText(
+              text: 'REPLY ALL',
+              icon: Icons.reply_all,
+            ),
+            color: Colors.white,
+            textColor: Colors.grey[400],
+            onPressed: _onReplyAllMessage,
+            height: 50.0,
+          ),
+          new MaterialButton(
+            child: _buildIconWithText(
+              text: 'FORWARD',
+              icon: Icons.forward,
+            ),
+            color: Colors.white,
+            textColor: Colors.grey[400],
+            onPressed: _onForwardMessage,
+            height: 50.0,
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ packages/widgets/test/email/message_list_item_test.dart
@@ -0,0 +1,98 @@
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:models/email/message.dart';
+import 'package:widgets/email/message_content.dart';
+import 'package:widgets/email/message_list_item.dart';
+
+void main() {
+  String profileUrl =
+    'https://raw.githubusercontent.com/dvdwasibi/DogsOfFuchsia/master/coco.jpg';
+  testWidgets(
+      'Test to see if tapping the header for a MessageListItem will call the'
+      'appropiate callback with given Message', (WidgetTester tester) async {
+    Message message = new Message(
+      sender: 'Coco Yang',
+      recipientList: <String>['David Yang'],
+      senderProfileUrl: profileUrl,
+      subject: 'Feed Me!!!',
+      text: "Woof Woof. I'm so hungry. You need to feed me!",
+      timestamp: new DateTime.now(),
+      isRead: true,
+    );
+
+    int taps = 0;
+
+    await tester.pumpWidget(new StatefulBuilder(
+        builder: (BuildContext context, StateSetter setState) {
+      return new Material(
+        child: new MessageListItem(
+          message: message,
+          onHeaderTap: (Message m) {
+            expect(m, message);
+            taps++;
+          },
+        ),
+      );
+    }));
+
+    expect(taps, 0);
+    await tester.tap(find.byType(ListItem));
+    expect(taps, 1);
+  });
+
+  testWidgets(
+      'Test to see if the MessageContent is not shown if a MessageListItem'
+      'is not expanded', (WidgetTester tester) async {
+    Message message = new Message(
+      sender: 'Coco Yang',
+      recipientList: <String>['David Yang'],
+      senderProfileUrl: profileUrl,
+      subject: 'Feed Me!!!',
+      text: "Woof Woof. I'm so hungry. You need to feed me!",
+      timestamp: new DateTime.now(),
+      isRead: true,
+    );
+
+    await tester.pumpWidget(new StatefulBuilder(
+        builder: (BuildContext context, StateSetter setState) {
+      return new Material(
+        child: new MessageListItem(
+          message: message,
+          isExpanded: false,
+        ),
+      );
+    }));
+
+    expect(find.byType(MessageContent), findsNothing);
+  });
+
+  testWidgets(
+      'Test to see if the MessageContent is shown if a MessageListItem'
+      'is expanded', (WidgetTester tester) async {
+    Message message = new Message(
+      sender: 'Coco Yang',
+      recipientList: <String>['David Yang'],
+      senderProfileUrl: profileUrl,
+      subject: 'Feed Me!!!',
+      text: "Woof Woof. I'm so hungry. You need to feed me!",
+      timestamp: new DateTime.now(),
+      isRead: true,
+    );
+
+    await tester.pumpWidget(new StatefulBuilder(
+        builder: (BuildContext context, StateSetter setState) {
+      return new Material(
+        child: new MessageListItem(
+          message: message,
+          isExpanded: true,
+        ),
+      );
+    }));
+
+    expect(find.byType(MessageContent), findsOneWidget);
+  });
+}
