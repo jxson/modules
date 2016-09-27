25d684c2e0586960a298d9cb57802fdcbffce824
Jason Campbell <jasoncampbell@google.com>
2016-09-26 19:11:29 -0700

    WIP, static definitions for fixtures.
    Change-Id: Iaca6372fc504d748e80c244b9d9a68e894db0c4f

---
+++ packages/models/lib/user/user.dart
@@ -0,0 +1,20 @@
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// TODO(jxson): Document this.
+class User {
+  /// TODO(jxson): Document this.
+  String name;
+  /// TODO(jxson): Document this.
+  String email;
+
+  /// TODO(jxson): Document this.
+  User({
+    this.name,
+    this.email }) {
+      print("User init");
+    assert(name != null);
+    assert(email != null);
+  }
+}
