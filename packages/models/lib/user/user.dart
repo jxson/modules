af33efc04e7155601b403313dcdca5e89641b7cc
David Yang <dayang@google.com>
2016-09-27 11:21:38 -0700

    feat(model): User Model based on Google Identity APIs
    based on: https://developers.google.com/identity/
    
    Jira Issue:
    SO-9 #start-review
    
    Change-Id: I6575c7143175dc8402c17b663567bc0396a981bb

---
+++ packages/models/lib/user/user.dart
@@ -0,0 +1,40 @@
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// Represents a Google User Account
+/// Fields are based off the data from the Google Identity API:
+/// https://developers.google.com/identity/
+class User {
+  /// Unique ID for user
+  String id;
+
+  /// Email address for user
+  String email;
+
+  /// Full name for user, Ex: John Doe
+  String name;
+
+  /// 'First name' for user, Ex: John
+  String givenName;
+
+  /// 'Last name' for user, Ex: Doe
+  String familyName;
+
+  /// URL for user avatar
+  String picture;
+
+  /// Location that user is associated with
+  String locale;
+
+  /// Constructor to create a new user
+  User({
+    this.id,
+    this.email,
+    this.name,
+    this.givenName,
+    this.familyName,
+    this.picture,
+    this.locale,
+  });
+}
