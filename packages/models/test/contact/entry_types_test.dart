b8919b7de64c9924899164a1e1cbcea385faca04
David Yang <dayang@google.com>
2016-10-05 16:24:39 -0700

    feat(ui): Contact Models
    Fleshing out Models needed to drive core Contact
    UI widgets.
    
    Roughly based on Google's Contact API:
    https://developers.google.com/google-apps/contacts/v3/
    
    SO-39 #done
    
    Change-Id: I1e8cf367587036598273078f8d2ae9ac3780e5a3

---
+++ packages/models/test/contact/entry_types_test.dart
@@ -0,0 +1,45 @@
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:models/contact/entry_types.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('EmailEntry', () {
+    test('toString()', () {
+      EmailEntry email = new EmailEntry(
+        label: 'Personal',
+        value: 'coco@puppy.cute',
+      );
+      expect(email.toString(), 'coco@puppy.cute');
+    });
+  });
+
+  group('AddressEntry', () {
+    test('toString()', () {
+      AddressEntry addressEntry = new AddressEntry(
+        street: '1842 N Shoreline Blvd',
+        city: 'Mountain View',
+        region: 'California',
+        postalCode: '94043',
+        country: 'United States of America',
+        countryCode: 'US',
+      );
+      expect(
+          addressEntry.toString(),
+          '1842 N Shoreline Blvd, '
+          'Mountain View, California, 94043, United States of America, US');
+    });
+  });
+
+  group('SocialNetworkEntry', () {
+    test('toString()', () {
+      SocialNetworkEntry socialNetworkEntry = new SocialNetworkEntry(
+        type: SocialNetworkType.twitter,
+        account: 'google',
+      );
+      expect(socialNetworkEntry.toString(), 'google');
+    });
+  });
+}
