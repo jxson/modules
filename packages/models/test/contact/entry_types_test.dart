ecf95a0b0c6d73771ae8d34f005b96fe21296055
David Yang <dayang@google.com>
2016-10-17 10:15:55 -0700

    feat(ui): Contact Details - Scaffolding
    Basic scaffolding and structure for the contact details widget.
    Includes basic layout, background image, alphatar and
    top level actions (call, email, text).
    
    Screenshot:
    https://fuchsia.atlassian.net/secure/attachment/10705/Screenshot_20161017-102401.png
    
    Change-Id: Iccf3fb001869e420d885adfe52e0b3567597e253

--- packages/models/test/contact/entry_types_test.dart
+++ packages/models/test/contact/entry_types_test.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'package:models/contact.dart';
+import 'package:models/user.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -40,6 +41,183 @@ void main() {
         account: 'google',
       );
       expect(socialNetworkEntry.toString(), 'google');
+    });
+  });
+
+  group('Contact', () {
+    group('primaryAddress', () {
+      test('Should return null if contact contains no addresses', () {
+        Contact contact = new Contact(
+          user: new User(
+            name: 'Coco',
+            email: 'Coco@cu.te',
+          ),
+        );
+        expect(contact.primaryAddress, null);
+      });
+      test('Should return first address if contact contains addresses', () {
+        Contact contact = new Contact(
+          user: new User(
+            name: 'Coco',
+            email: 'Coco@cu.te',
+          ),
+          addresses: <AddressEntry>[
+            new AddressEntry(
+              label: 'Work',
+              city: 'Mountain View',
+            ),
+            new AddressEntry(
+              label: 'Home',
+              city: 'San Francisco',
+            ),
+          ],
+        );
+        expect(contact.primaryAddress, contact.addresses[0]);
+      });
+    });
+    group('primaryEmail', () {
+      test('Should return null if contact contains no emails', () {
+        Contact contact = new Contact(
+          user: new User(
+            name: 'Coco',
+            email: 'Coco@cu.te',
+          ),
+        );
+        expect(contact.primaryEmail, null);
+      });
+      test('Should return first address if contact contains addresses', () {
+        Contact contact = new Contact(
+          user: new User(
+            name: 'Coco',
+            email: 'Coco@cu.te',
+          ),
+          emails: <EmailEntry>[
+            new EmailEntry(
+              label: 'Work',
+              value: 'coco@work',
+            ),
+            new EmailEntry(
+              label: 'Home',
+              value: 'coco@home',
+            ),
+          ],
+        );
+        expect(contact.primaryEmail, contact.emails[0]);
+      });
+    });
+    group('primaryPhoneNumber', () {
+      test('Should return null if contact contains no phone numbers', () {
+        Contact contact = new Contact(
+          user: new User(
+            name: 'Coco',
+            email: 'Coco@cu.te',
+          ),
+        );
+        expect(contact.primaryPhoneNumber, null);
+      });
+      test('Should return first phone number if contact contains phone numbers',
+          () {
+        Contact contact = new Contact(
+          user: new User(
+            name: 'Coco',
+            email: 'Coco@cu.te',
+          ),
+          phoneNumbers: <PhoneEntry>[
+            new PhoneEntry(
+              label: 'Work',
+              number: '13371337',
+            ),
+            new PhoneEntry(
+              label: 'Home',
+              number: '101010101',
+            ),
+          ],
+        );
+        expect(contact.primaryPhoneNumber, contact.phoneNumbers[0]);
+      });
+    });
+    group('regionPreview', () {
+      test('Should return null if contact has no primary address', () {
+        Contact contact = new Contact(
+          user: new User(
+            name: 'Coco',
+            email: 'Coco@cu.te',
+          ),
+        );
+        expect(contact.regionPreview, null);
+      });
+      test(
+          'Should return null if contact has primary address but no city nor region is given',
+          () {
+        Contact contact = new Contact(
+          user: new User(
+            name: 'Coco',
+            email: 'Coco@cu.te',
+          ),
+          addresses: <AddressEntry>[
+            new AddressEntry(
+              country: 'USA',
+              label: 'Work',
+            )
+          ],
+        );
+        expect(contact.regionPreview, null);
+      });
+      test(
+          'Should return city if contact has primary address with a city and no region',
+          () {
+        Contact contact = new Contact(
+          user: new User(
+            name: 'Coco',
+            email: 'Coco@cu.te',
+          ),
+          addresses: <AddressEntry>[
+            new AddressEntry(
+              city: 'Mountain View',
+              country: 'USA',
+              label: 'Work',
+            )
+          ],
+        );
+        expect(contact.regionPreview, 'Mountain View');
+      });
+      test(
+          'Should return region if contact has primary address with a region and no city',
+          () {
+        Contact contact = new Contact(
+          user: new User(
+            name: 'Coco',
+            email: 'Coco@cu.te',
+          ),
+          addresses: <AddressEntry>[
+            new AddressEntry(
+              region: 'CA',
+              country: 'USA',
+              label: 'Work',
+            )
+          ],
+        );
+        expect(contact.regionPreview, 'CA');
+      });
+      test(
+          'Should return city, region if contact has primary address with a region and city',
+          () {
+        Contact contact = new Contact(
+          user: new User(
+            name: 'Coco',
+            email: 'Coco@cu.te',
+          ),
+          addresses: <AddressEntry>[
+            new AddressEntry(
+              city: 'Mountain View',
+              region: 'CA',
+              country: 'USA',
+              label: 'Work',
+            )
+          ],
+        );
+        expect(contact.regionPreview, 'Mountain View, CA');
+      });
     });
   });
 }
