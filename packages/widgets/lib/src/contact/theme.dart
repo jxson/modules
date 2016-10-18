eb87532feb79eea37e38a6a66355280ab571098d
David Yang <dayang@google.com>
2016-10-18 14:31:15 -0700

    feat(ui): Using ThemeData in ContactDetails
    Create a seperate ThemeData file for all contact widgets.
    This theme will be "injected" at the gallery and should
    eventually be part of the buildContext as the widgets
    are converted to modules.
    
    Change-Id: Ibf68c3cf7b4526b846550e40670812e8baa1a063

---
+++ packages/widgets/lib/src/contact/theme.dart
@@ -0,0 +1,12 @@
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+/// ThemeData used across all contact widgets
+ThemeData contactTheme = new ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.orange[700],
+  buttonColor: Colors.orange[700],
+);
