f27bbf67a87709840b9138314c978ec65d4790df
YoungSeok Yoon <youngseokyoon@google.com>
2016-10-18 19:09:40 -0700

    feat(secure_store): add a simple mock API for the secure store service
    The first .mojom based mock API for a secure store service. For now,
    it is assumed that the store is already scoped to the current user and
    the specific module that is using this service.
    
    It is a really simple string key/value store, but we could add more
    features in the future.
    
    SO-59 #comment initial version
    
    Change-Id: I566f4537cb839b8c150ee500198658bb3194e5e5

--- packages/util/lib/local_storage.dart
+++ packages/util/lib/local_storage.dart
@@ -8,37 +8,43 @@ import 'dart:io';
 
 import 'package:flutter/services.dart';
 
+const String _dataFileName = 'storage.json';
+
+/// The default [LocalStorage] instance that other packages can import and use.
+const LocalStorage localStorage = const LocalStorage(_dataFileName);
+
 /// A class for locally persist data on device. This class can store any string
 /// key-value pairs and retrieve them later. The current implementation is not
 /// particularly efficient, and this is intended.
-///
-// TODO(youngseokyoon): Replace this with a mock Secure storage API.
-// https://fuchsia.atlassian.net/browse/SO-87
 class LocalStorage {
-  static const String _dataFileName = 'storage.json';
+  /// The file name to be used for the local storage instance.
+  final String dataFileName;
+
+  /// Create a new local storage instance with the given file name.
+  const LocalStorage(this.dataFileName);
 
   /// Gets the value for the given [key].
-  static Future<String> get(String key) async {
+  Future<String> get(String key) async {
     _Store store = await _load();
     return store[key];
   }
 
   /// Associates the given [value] with the given [key].
-  static Future<Null> put(String key, String value) async {
+  Future<Null> put(String key, String value) async {
     _Store store = await _load();
     store[key] = value;
     await store.save();
   }
 
   /// Deletes the given [key] and its associated value.
-  static Future<Null> remove(String key) async {
+  Future<Null> remove(String key) async {
     _Store store = await _load();
     store.remove(key);
     await store.save();
   }
 
   /// Creates and initialize the store.
-  static Future<_Store> _load() async {
+  Future<_Store> _load() async {
     File file = new File(await _getFilePath());
 
     _Store store = new _Store(file);
@@ -48,9 +54,9 @@ class LocalStorage {
   }
 
   /// Gets the full path of the data file.
-  static Future<String> _getFilePath() async {
+  Future<String> _getFilePath() async {
     String dir = (await PathProvider.getApplicationDocumentsDirectory()).path;
-    return '$dir/$_dataFileName';
+    return '$dir/$dataFileName';
   }
 }
 
