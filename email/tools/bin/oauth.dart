e4ad33fe0d0828a5520e46169d560c251137308c
Jason Campbell <jasoncampbell@google.com>
2016-11-13 17:48:32 -0800

    Adds working auth client baking.
    Change-Id: Ieaeb7a6f21db1e3b734e55c69bbbdf19feb2ced4

--- email/tools/bin/oauth.dart
+++ email/tools/bin/oauth.dart
@@ -3,7 +3,9 @@
 // found in the LICENSE file.
 
 import 'dart:async';
+import 'dart:io';
 
+import 'package:path/path.dart' as path;
 import 'package:http/http.dart' as http;
 import 'package:googleapis_auth/auth_io.dart';
 import 'package:tools/config.dart';
@@ -21,9 +23,45 @@ Future<Null> main(List<String> args) async {
 
   AccessCredentials credentials =
       await obtainAccessCredentialsViaUserConsent(id, scopes, client, prompt);
-
-  print('SUCCESS $credentials');
   client.close();
+
+  String source = '''
+import 'package:http/http.dart' as http;
+import 'package:googleapis_auth/auth_io.dart';
+
+const String _kOauthID =
+    '$identifier';
+const String _kOauthSecret = '$secret';
+const String _kTokenData =
+    '${credentials.accessToken.data}';
+final DateTime _kTokenExpiry = DateTime.parse('${credentials.accessToken.expiry}');
+const String _kRefreshToken = '${credentials.refreshToken}';
+const List<String> _kScopes = const <String>[
+  'https://www.googleapis.com/auth/gmail.modify'
+];
+
+/// Returns an auto refreshing auth client for REST API requests.
+///
+/// NOTE: Some special attention may be needed for closing both the returned
+/// client and the baseClient.
+AutoRefreshingAuthClient client() {
+  AccessToken token = new AccessToken('Bearer', _kTokenData, _kTokenExpiry);
+  AccessCredentials credentials =
+      new AccessCredentials(token, _kRefreshToken, _kScopes);
+  ClientId id = new ClientId(_kOauthID, _kOauthSecret);
+  http.Client baseClient = new http.Client();
+  return autoRefreshingClient(id, credentials, baseClient);
+}
+''';
+
+  String dirname = path.normalize(path.absolute('..'));
+  String filename =
+      path.join(dirname, 'service', 'lib', 'src', 'api', 'client.dart');
+
+  File file = new File(filename);
+  await file.writeAsString(source);
+
+  print('created: $file');
 }
 
 void prompt(String url) {
