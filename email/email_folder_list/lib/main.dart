bcfaafb354c45b12bfdeb607d462173485a8c756
YoungSeok Yoon <youngseokyoon@google.com>
2016-11-15 13:57:35 -0800

    feat(modular): initial flutter-based email modules
    This CL contains the basic BUILD.gn files and preliminary flutter
    module implementation for the email experience.
    
    Change-Id: I8d15a70102504a436f2f48c6590c406f7784588c

---
+++ email/email_folder_list/lib/main.dart
@@ -0,0 +1,80 @@
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:apps.modular.lib.app.dart/app.dart';
+import 'package:apps.modular.services.application/service_provider.fidl.dart';
+import 'package:apps.modular.services.story/link.fidl.dart';
+import 'package:apps.modular.services.story/module.fidl.dart';
+import 'package:apps.modular.services.story/module_controller.fidl.dart';
+import 'package:apps.modular.services.story/story.fidl.dart';
+import 'package:apps.mozart.lib.flutter/child_view.dart';
+import 'package:apps.mozart.services.views/view_manager.fidl.dart';
+import 'package:apps.mozart.services.views/view_provider.fidl.dart';
+import 'package:apps.mozart.services.views/view_token.fidl.dart';
+import 'package:flutter/material.dart';
+import 'package:lib.fidl.dart/bindings.dart';
+
+final ApplicationContext _context = new ApplicationContext.fromStartupInfo();
+
+void _log(String msg) {
+  print('[Email Folder List Module] $msg');
+}
+
+/// An implementation of the [Module] interface.
+class ModuleImpl extends Module {
+  final ModuleBinding _binding = new ModuleBinding();
+
+  /// Bind an [InterfaceRequest] for a [Module] interface to this object.
+  void bind(InterfaceRequest<Module> request) {
+    _binding.bind(this, request);
+  }
+
+  /// Implementation of the Initialize(Story story, Link link) method.
+  @override
+  void initialize(
+    InterfaceHandle<Story> storyHandle,
+    InterfaceHandle<Link> linkHandle,
+  ) {
+    _log('ModuleImpl::initialize call');
+
+    StoryProxy story = new StoryProxy();
+    story.ctrl.bind(storyHandle);
+
+    LinkProxy link = new LinkProxy();
+    link.ctrl.bind(linkHandle);
+
+    // Do something with the story / link.
+  }
+
+  @override
+  void stop(void callback()) {
+    _log('ModuleImpl::stop call');
+
+    // Do some clean up here.
+
+    // Invoke the callback to signal that the clean-up process is done.
+    callback();
+  }
+}
+
+/// Main entry point to the email folder list module.
+void main() {
+  _log('Email folder list module started with context: $_context');
+
+  /// Add [ModuleImpl] to this application's outgoing ServiceProvider.
+  _context.outgoingServices.addServiceForName(
+    (request) {
+      _log('Received binding request for Module');
+      new ModuleImpl().bind(request);
+    },
+    Module.serviceName,
+  );
+
+  runApp(new MaterialApp(
+    title: "Email Folder List",
+    home: new Text('I am the email folder list module!'),
+  ));
+}
