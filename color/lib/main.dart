a2e92afaea97b29ec369cc5b3c209c741f75d6cc
Andrew Wilson <apwilson@google.com>
2016-11-22 10:03:32 -0800

    A module that displays a color.
    Change-Id: If5370a002e8ca96940247b9dd004126cbebe297b

---
+++ color/lib/main.dart
@@ -0,0 +1,81 @@
+// Copyright 2016 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:apps.modular.lib.app.dart/app.dart';
+import 'package:apps.modular.services.application/service_provider.fidl.dart';
+import 'package:apps.modular.services.story/link.fidl.dart';
+import 'package:apps.modular.services.story/module.fidl.dart';
+import 'package:apps.modular.services.story/story.fidl.dart';
+import 'package:flutter/widgets.dart';
+import 'package:lib.fidl.dart/bindings.dart';
+
+const Color _kTurquoise = const Color(0xFF1DE9B6);
+
+final ApplicationContext _context = new ApplicationContext.fromStartupInfo();
+
+void _log(String msg) {
+  print('[Color Module] $msg');
+}
+
+/// An implementation of the [Module] interface.
+class ModuleImpl extends Module {
+  final ModuleBinding _binding = new ModuleBinding();
+
+  /// [Story] service provided by the framework.
+  final StoryProxy story = new StoryProxy();
+
+  /// [Link] service provided by the framework.
+  final LinkProxy link = new LinkProxy();
+
+  /// Bind an [InterfaceRequest] for a [Module] interface to this object.
+  void bind(InterfaceRequest<Module> request) {
+    _binding.bind(this, request);
+  }
+
+  /// Implementation of the Initialize(Story story, Link link) method.
+  @override
+  void initialize(
+    InterfaceHandle<Story> storyHandle,
+    InterfaceHandle<Link> linkHandle,
+    InterfaceHandle<ServiceProvider> incomingServices,
+    InterfaceRequest<ServiceProvider> outgoingServices,
+  ) {
+    _log('ModuleImpl::initialize call');
+
+    story.ctrl.bind(storyHandle);
+    link.ctrl.bind(linkHandle);
+  }
+
+  @override
+  void stop(void callback()) {
+    _log('ModuleImpl::stop call');
+
+    // Do some clean up here.
+
+    // Invoke the callback to signal that the clean-up process is done.
+    callback();
+  }
+}
+
+/// Main entry point to the color module.
+void main() {
+  _log('Color module started with context: $_context');
+
+  /// Add [ModuleImpl] to this application's outgoing ServiceProvider.
+  _context.outgoingServices.addServiceForName(
+    (InterfaceRequest<Module> request) {
+      _log('Received binding request for Module');
+      new ModuleImpl().bind(request);
+    },
+    Module.serviceName,
+  );
+
+  runApp(
+    new Container(
+      decoration: new BoxDecoration(
+        backgroundColor: _kTurquoise,
+      ),
+    ),
+  );
+}
