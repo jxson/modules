802e0d4508523b71477f88ac7ddfd46efd89976a
YoungSeok Yoon <youngseokyoon@google.com>
2016-11-22 15:37:27 -0800

    feat(email): sample fidl service exchange logic
    This CL demonstrates how modules can exchange their FIDL services.
    At a high level, this is what's happening.
    
    1. email_service module provides an implementation of ServiceProvider,
       which can serve the Threads implementation.
    2. email_story (QB module) gets that ServiceProvider, duplicates the
       handle, and passes the wrapped ServiceProvider down to the UI
       modules (ideally this should only go to the session module, but I
       believe Alan is working on that part.)
    3. email_nav gets the ServiceProvider as the incomingServices, and
       connects to the Threads service.
    4. email_nav can now call any methods on the Threads interface.
    
    Change-Id: I5c4afaeaeb8b420370c27e85031bcde223228375

--- email/email_nav/lib/main.dart
+++ email/email_nav/lib/main.dart
@@ -7,6 +7,7 @@ import 'package:apps.modular.services.application/service_provider.fidl.dart';
 import 'package:apps.modular.services.story/link.fidl.dart';
 import 'package:apps.modular.services.story/module.fidl.dart';
 import 'package:apps.modular.services.story/story.fidl.dart';
+import 'package:apps.modules.email.email_service/threads.fidl.dart' as es;
 import 'package:flutter/material.dart';
 import 'package:lib.fidl.dart/bindings.dart';
 import 'package:models/email.dart';
@@ -23,6 +24,12 @@ void _log(String msg) {
 class ModuleImpl extends Module {
   final ModuleBinding _binding = new ModuleBinding();
 
+  /// [ServiceProvider] given from the parent module.
+  final ServiceProviderProxy incomingServices = new ServiceProviderProxy();
+
+  /// [Threads] service obtained from the incoming [ServiceProvider].
+  final es.ThreadsProxy threadsService = new es.ThreadsProxy();
+
   /// Bind an [InterfaceRequest] for a [Module] interface to this object.
   void bind(InterfaceRequest<Module> request) {
     _binding.bind(this, request);
@@ -37,15 +44,27 @@ class ModuleImpl extends Module {
     InterfaceRequest<ServiceProvider> outgoingServices,
   ) {
     _log('ModuleImpl::initialize call');
+
+    // This `serviceProvider` is originally from the `email_service`, which can
+    // serve the `Threads` interface. Use `connectToService` function to get the
+    // actual `Threads` interface.
+    incomingServices.ctrl.bind(incomingServicesHandle);
+    connectToService(incomingServices, threadsService.ctrl);
+
+    // Call the function defined in threads.fidl, and print the received value.
+    threadsService.inbox(0, (List<es.Thread> threads) {
+      _log('Received threads from the email service');
+      threads.forEach((es.Thread t) {
+        _log('id: ${t.id}');
+      });
+    });
   }
 
   @override
   void stop(void callback()) {
     _log('ModuleImpl::stop call');
-
-    // Do some clean up here.
-
-    // Invoke the callback to signal that the clean-up process is done.
+    threadsService.ctrl.close();
+    incomingServices.ctrl.close();
     callback();
   }
 }
--- email/email_service/lib/src/threads_impl.dart
+++ email/email_service/lib/src/threads_impl.dart
@@ -2,15 +2,26 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:apps.modules.email.email_service/threads.fidl.dart' as fidl;
+import 'package:apps.modules.email.email_service/threads.fidl.dart' as es;
+import 'package:lib.fidl.dart/bindings.dart';
 
 /// Implementation for email_service.
-class ThreadsImpl extends fidl.Threads {
+class ThreadsImpl extends es.Threads {
+  final es.ThreadsBinding _binding = new es.ThreadsBinding();
+
+  /// Binds this implementation to the incoming [InterfaceRequest].
+  ///
+  /// This should only be called once. In other words, a new [ThreadsImpl]
+  /// object needs to be created per interface request.
+  void bind(InterfaceRequest<es.Threads> request) {
+    _binding.bind(this, request);
+  }
+
   @override
-  void inbox(int max, void callback(List<fidl.Thread> threads)) {
-    fidl.Thread thread = new fidl.Thread();
+  void inbox(int max, void callback(List<es.Thread> threads)) {
+    es.Thread thread = new es.Thread();
     thread.id = 'thread-example';
-    List<fidl.Thread> threads = <fidl.Thread>[thread];
+    List<es.Thread> threads = <es.Thread>[thread];
     callback(threads);
   }
 }
