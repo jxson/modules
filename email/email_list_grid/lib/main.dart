8a6048389fc3a1e1a0be833eebed031c4667b5ce
YoungSeok Yoon <youngseokyoon@google.com>
2016-11-25 20:29:20 -0800

    feat(email_list_grid): use the flux-version email inbox screen
    Change-Id: I1aa5d2a0f3c6c99917932b58d8822b18fe26a1ae

--- email/email_list_grid/lib/main.dart
+++ email/email_list_grid/lib/main.dart
@@ -8,7 +8,6 @@ import 'package:email_session_client/client.dart';
 import 'package:email_session_store/email_session_store.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_flux/flutter_flux.dart';
-import 'package:models/email.dart';
 import 'package:widgets/email.dart';
 
 const String _moduleName = 'email_grid_list';
@@ -19,26 +18,6 @@ void _log(String msg) {
   print('[$_moduleName] $msg');
 }
 
-/// Temporary widget class for the email list module.
-class EmailGridScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Material(
-      child: new ThreadGridLayout(
-        children: _buildThreadGridItems(),
-      ),
-    );
-  }
-
-  List<Widget> _buildThreadGridItems() {
-    return <Widget>[
-      new ThreadGridItem(thread: new MockThread()),
-      new ThreadGridItem(thread: new MockThread()),
-      new ThreadGridItem(thread: new MockThread()),
-    ];
-  }
-}
-
 void _created(EmailSessionModule module) {
   _module = module;
 }
@@ -46,6 +25,13 @@ void _created(EmailSessionModule module) {
 void _initialize(es.EmailSession service, EmailSessionLinkStore store) {
   // HACK: Global reference must be set before store is accessed by widgets.
   kEmailSessionStoreToken = new StoreToken(store);
+
+  runApp(new MaterialApp(
+    title: 'Email List Grid Module',
+    home: new EmailInboxScreen(style: InboxStyle.gridView),
+    theme: new ThemeData(primarySwatch: Colors.blue),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
 void _stop(void callback()) {
@@ -56,10 +42,4 @@ void _stop(void callback()) {
 void main() {
   _log('Email grid list module started with context: $_context');
   addEmailSessionModule(_context, _moduleName, _created, _initialize, _stop);
-  runApp(new MaterialApp(
-    title: 'Email List Grid Module',
-    home: new EmailGridScreen(),
-    theme: new ThemeData(primarySwatch: Colors.blue),
-    debugShowCheckedModeBanner: false,
-  ));
 }
