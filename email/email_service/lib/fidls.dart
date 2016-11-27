f4639826189c57542025a74256b59d689fdaf53d
Jason Campbell <jasoncampbell@google.com>
2016-11-26 18:00:09 -0800

    Consolidated.
    Change-Id: I04c06228d1b33bcc8d774096db250d245d873402

--- email/email_service/lib/fidls.dart
+++
@@ -1,7 +0,0 @@
-// Copyright 2016 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-export 'package:apps.modules.email.email_service/labels.fidl.dart';
-export 'package:apps.modules.email.email_service/threads.fidl.dart';
-export 'package:apps.modules.email.email_service/users.fidl.dart';
