136e893e6ff0e5e399c05ffd6168b1eccdf4c657
YoungSeok Yoon <youngseokyoon@google.com>
2017-01-31 16:12:44 +0900

    feat(widget_specs): add @ConfigKey annotation for config value retrieval
    This adds yet another annotation @ConfigKey, which can be used to
    indicate a certain widget parameter should be retrieved from the
    config file.
    
    Example:
    
        class YoutubeVideo extends StatelessWidget {
          // ...
          final String apiKey;
          // ...
    
          YoutubeVideo({
            // ...
            @required @ConfigKey('google_api_key') this.apiKey,
            // ...
          })
          // ...
        }
    
    When a parameter has this annotation, the generator can inject code
    for retrieving the specified config value from the Config object.
    
    SO-175 #done
    
    Change-Id: I1c2bcb3995f278daa30567da3434c207e1c263c1

--- packages/widgets_meta/lib/widgets_meta.dart
+++ packages/widgets_meta/lib/widgets_meta.dart
@@ -2,5 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+export 'src/config_key.dart';
 export 'src/example_value.dart';
 export 'src/generator.dart';
