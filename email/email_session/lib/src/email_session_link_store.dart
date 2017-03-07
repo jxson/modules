a6d5b97795eb694447904532332db2b2307c481a
Jim Beveridge <jimbe@chromium.org>
2017-03-07 12:46:04 -0800

    Removed json log spew
    Change-Id: Ia9917cf1d7d4007b69fe2ba4d26d9953f99632b6

--- email/email_session/lib/src/email_session_link_store.dart
+++ email/email_session/lib/src/email_session_link_store.dart
@@ -35,7 +35,7 @@ class EmailSessionLinkStore extends Store implements EmailSessionStore {
   }
 
   void _onUpdate(dynamic json) {
-    _log('Received _onUpdate $json');
+    _log('Received _onUpdate');
     if (json is! Map) {
       return null;
     }
